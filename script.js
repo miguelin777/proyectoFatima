154b76e20defa9d8d03021a4547f54439851cc93
David Martinez <321davidmr123@gmail.com>
2023-05-09 10:35:26 -0600

    nav is ready on beta

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ if (cards.length > 0) {
     observer.observe(card);
   });
 }
+// let tring = document.getElementsByClassName("show");
+// console.log(tring);
+// window.addEventListener("click",function() {
+  //   if (tring.classList == "offcanvas offcanvas-end w fade show"){
+    //     tring2.classList.toggle("fade2", window.addEventListener("click"));
+    //   }
+    // let tring2 = document.getElementById("offcanvasNavbar");
+// })
+// window.addEventListener(observer.observe(),function() {
+//   let clase = "offcanvas offcanvas-end w fade show"
+//   let tring2 = document.getElementById("offcanvasNavbar");
+//   let compara = tring2.attributes[0].value;
+//   console.log(tring2.attributes[0].value);
+//   if (compara == clase){
+//     tring2.classList.toggle("fade2")
+//   }
+// })
+// console.log(tring2);
+// if (tring.classList == "offcanvas offcanvas-end w fade show"){
+//   tring2.classList.toggle("fade2");
+// }
+window.addEventListener("scroll", function(){
+  let nav = document.getElementById("toid");
+  let li = document.getElementById("it");
+  nav.classList.toggle("asd", window.scrollY > 40)
+  li.classList.toggle("letras", window.scrollY > 40)
+})
